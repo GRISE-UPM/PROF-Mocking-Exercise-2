e9824a08d63516c5e47be5d21181837f9ce36546
Riku <rperisacedo@gmail.com>
2023-11-30 10:56:41 +0100

    Inicialización externa de las variables globales

--- src/test/java/es/grise/upm/profundizacion/mocking/exercise2/CarTest.java
+++ src/test/java/es/grise/upm/profundizacion/mocking/exercise2/CarTest.java
@@ -10,16 +10,22 @@ import org.junit.jupiter.api.Test;
 
 
 public class CarTest {
+
+	// Inicialización de variables globales
+	private Logger logger = mock(Logger.class);
+	private Speedometer speedometer = mock(Speedometer.class);
+	private Gearbox gearbox = mock(Gearbox.class);
+	private Time time = mock(Time.class);
+	private EngineController engineController = new EngineController(logger, speedometer, gearbox, time);
+
+	// Inicialización de constante para el formato de la fecha
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
     // Test para probar que el mensaje de log se genera en el formato correcto (recordGear())
 	@Test
 	public void testRecordGear() {
-		Logger logger = mock(Logger.class);
-		Speedometer speedometer = mock(Speedometer.class);
-		Gearbox gearbox = mock(Gearbox.class);
-		Time time = mock(Time.class);
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		EngineController engineController = new EngineController(logger, speedometer, gearbox, time);
-		when(time.getCurrentTime()).thenReturn(new Timestamp(0));
+		when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
 	
 		engineController.recordGear(GearValues.FIRST);
 	
@@ -29,29 +35,19 @@ public class CarTest {
     // Test para probar que se calcula correctamente la velocidad instantánea (getInstantaneousSpeed())
 	@Test
 	public void testGetInstantaneousSpeed() {
-		Logger logger = mock(Logger.class);
-		Speedometer speedometer = mock(Speedometer.class);
-		Gearbox gearbox = mock(Gearbox.class);
-		Time time = mock(Time.class);
-		EngineController engineController = new EngineController(logger, speedometer, gearbox, time);
 		when(speedometer.getSpeed()).thenReturn(10.0);
 	
 		double speed = engineController.getInstantaneousSpeed();
+		double expectedSpeed = 10.0;
 	
-		assertEquals(10.0, speed);
+		assertEquals(expectedSpeed, speed);
 	}
 
 	// Test para probar que el método adjustGear() funciona correctamente invoca 3 veces a getInstantaneousSpeed()
 	@Test
 	public void tresinvocacionesadjustGear(){
-		Logger logger = mock(Logger.class);
-		Speedometer speedometer = mock(Speedometer.class);
-		Gearbox gearbox = mock(Gearbox.class);
-		Time time = mock(Time.class);
-		EngineController engineController = new EngineController(logger, speedometer, gearbox, time);
-
 		when(speedometer.getSpeed()).thenReturn(10.0);
-		when(time.getCurrentTime()).thenReturn(new Timestamp(0));
+		when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
 
 		engineController.adjustGear();
 
@@ -61,15 +57,8 @@ public class CarTest {
     //  Test para probar que el método adjustGear() mete la nueva marcha en recordGear()
 	@Test
 	public void loguearNuevaMarcha(){
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Logger logger = mock(Logger.class);
-		Speedometer speedometer = mock(Speedometer.class);
-		Gearbox gearbox = mock(Gearbox.class);
-		Time time = mock(Time.class);
-		EngineController engineController = new EngineController(logger, speedometer, gearbox, time);
-
 		when(speedometer.getSpeed()).thenReturn(10.0);
-		when(time.getCurrentTime()).thenReturn(new Timestamp(0));
+		when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
 
 		engineController.adjustGear();
 
@@ -79,14 +68,8 @@ public class CarTest {
     // Test para probar que el método adjustGear() asigna correctamente la nueva marcha en setGear()
 	@Test
 	public void asignarNuevaMarcha(){
-		Logger logger = mock(Logger.class);
-		Speedometer speedometer = mock(Speedometer.class);
-		Gearbox gearbox = mock(Gearbox.class);
-		Time time = mock(Time.class);
-		EngineController engineController = new EngineController(logger, speedometer, gearbox, time);
-
 		when(speedometer.getSpeed()).thenReturn(10.0);
-		when(time.getCurrentTime()).thenReturn(new Timestamp(0));
+		when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
 
 		engineController.adjustGear();
 
