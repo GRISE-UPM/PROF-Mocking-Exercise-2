8416df875bf45e166e632efd514c2e2bc6525464
Eric García <114096503+Palid0@users.noreply.github.com>
2023-11-30 17:52:22 +0100

    Añadidos tests de mockito

---
+++ src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
@@ -0,0 +1,107 @@
+package es.grise.upm.profundizacion.mocking.exercise2;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Timestamp;
+
+public class EngineControllerTest {
+
+    @Test
+    public void correctLogFormat(){
+        Logger mockLogger = mock(Logger.class);
+        Speedometer mockSpeedometer = mock(Speedometer.class);
+        Gearbox mockGearbox = mock(Gearbox.class);
+        Time mockTime = mock(Time.class);
+
+        EngineController realController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime);
+
+        when(mockTime.getCurrentTime()).thenReturn(new Timestamp(1701029476L)); //fake time value
+        realController.recordGear(GearValues.FIRST); //call the real recorder method, so as to call "log"
+
+        verify(mockLogger).log("1970-01-20 17:30:29 Gear changed to FIRST"); //check that mockLogger's log call matches the fake time value
+    }
+
+    @Test
+    public void correctInstantSpeed(){
+        Logger mockLogger = mock(Logger.class);
+        Speedometer mockSpeedometer = mock(Speedometer.class);
+        Gearbox mockGearbox = mock(Gearbox.class);
+        Time mockTime = mock(Time.class);
+
+        EngineController realController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime); //Real Controller
+        EngineController mockController = mock(EngineController.class); //Mock Controller
+
+        when(mockController.getInstantaneousSpeed()).thenReturn(100.0); //set-up for the mock
+
+        when(mockSpeedometer.getSpeed()).thenReturn(0.0, 300.0, 0.0);//set-up for the real controller
+
+        //Verify that getInstantaneousSpeed returned the right value:
+        assertEquals(mockController.getInstantaneousSpeed(), realController.getInstantaneousSpeed()); //Compare real vs mock execution
+
+    }
+
+    @Test
+    public void correctCallsGetSpeed(){
+        Logger mockLogger = mock(Logger.class);
+        Speedometer mockSpeedometer = mock(Speedometer.class);
+        Gearbox mockGearbox = mock(Gearbox.class);
+        Time mockTime = mock(Time.class);
+        Timestamp mockTimestamp = mock(Timestamp.class);
+
+        EngineController realController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime);
+        EngineController spyController = spy(realController); //A spy, so as to use actually use methods 
+
+        when(mockTime.getCurrentTime()).thenReturn(mockTimestamp); //Necessary so that recordGear does not fail
+
+        // Call adjustGear 3 times
+        spyController.adjustGear();
+        spyController.adjustGear();
+        spyController.adjustGear();
+
+       verify(spyController, times(3)).getInstantaneousSpeed(); //make sure getInstantaneousSpeed was called 3 times
+    }
+
+    @Test
+    public void correctRecordGear(){
+        Logger mockLogger = mock(Logger.class);
+        Speedometer mockSpeedometer = mock(Speedometer.class);
+        Gearbox mockGearbox = mock(Gearbox.class);
+        Time mockTime = mock(Time.class);
+
+        when(mockTime.getCurrentTime()).thenReturn(new Timestamp(1701029476L)); //fake time value
+
+        EngineController realController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime); //Real Controller
+        EngineController spyController = spy(realController); //Spy Controller
+
+        spyController.adjustGear(); // Call adjustGear
+
+        verify(spyController, times(1)).recordGear(GearValues.FIRST); // Verify that recordGear has been called
+    }
+
+    @Test
+    public void correctSetGear(){
+        Logger mockLogger = mock(Logger.class);
+        Speedometer mockSpeedometer = mock(Speedometer.class);
+        Gearbox mockGearbox = mock(Gearbox.class);
+        Time mockTime = mock(Time.class);
+
+        when(mockTime.getCurrentTime()).thenReturn(new Timestamp(1701029476L)); //fake time value
+
+        EngineController realController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime);
+        EngineController spyController = spy(realController); //Spy Controller
+
+        spyController.adjustGear(); // Call adjustGear
+
+        verify(spyController, times(1)).setGear(GearValues.FIRST); //check that the setGear method was called
+        
+        ArgumentCaptor<GearValues> argumentCaptor = ArgumentCaptor.forClass(GearValues.class); //set up an argument captor
+        verify(spyController).setGear(argumentCaptor.capture()); //spy on the execution of setGear, and have its argument be captured
+
+        GearValues value = argumentCaptor.getValue(); // the captured value has to be the one used by the controller, "GearValues.FIRST"
+        assertEquals(value, GearValues.FIRST); //Verify that the GearValue set was the correct one
+    }
+}
