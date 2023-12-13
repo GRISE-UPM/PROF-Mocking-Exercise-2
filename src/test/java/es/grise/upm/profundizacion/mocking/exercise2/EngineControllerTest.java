d3f37ae90841f4bfc128b7e898234cdd303cecd2
LuisBMI <luisbmi55@gmail.com>
2023-12-13 10:36:24 +0100

    tests implemented

--- src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
+++ src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
@@ -4,13 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import java.text.SimpleDateFormat;
+import java.sql.Timestamp;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,7 +25,8 @@ public class EngineControllerTest {
     Time time;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() 
+    {
         logger = mock(Logger.class);
         speedometer = mock(Speedometer.class);
         gearbox = mock(Gearbox.class);
@@ -36,27 +38,56 @@ public class EngineControllerTest {
 
     //a) The log message has the correct format (recordGear() method).
     @Test
-    public void testRecordGear() {
-
+    public void testRecordGear() 
+    {
+        when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
         engineController.recordGear(GearValues.FIRST);
-        verify(logger).log("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$\r\n" + 
-                " Gear changed to FIRST");
+        String expectedLogPattern = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} Gear changed to FIRST";
+        verify(logger).log(matches(expectedLogPattern));
         verifyNoMoreInteractions(logger);
     }
 
     //b) The instantaneous speed is correctly calculated (method getInstantaneousSpeed()).
     @Test
-    public void testGetInstantaneousSpeed() {
+    public void testGetInstantaneousSpeed() 
+    {
         when(speedometer.getSpeed()).thenReturn(10.0);
         assertEquals( 10.0, engineController.getInstantaneousSpeed());
-        verify(speedometer).getSpeed();
+        verify(speedometer, times(3)).getSpeed();
         verifyNoMoreInteractions(speedometer);
     }
 
     //c) The adjustGear method invokes the getInstantaneousSpeed() method exactly three times.
     @Test
-    public void testAdjustGear() {
+    public void testAdjustGear() 
+    {
+        when(speedometer.getSpeed()).thenReturn(10.0);
         engineController.adjustGear();
+        verify(speedometer, times(3)).getSpeed();
         verifyNoMoreInteractions(speedometer);
+    }
+
+    //d) The adjustGear method logs the new gear (method recordGear()).
+    //Igual que el test a), pero esta vez con el método adjustGear().
+    @Test
+    public void testAdjustGearLogsNewGear() 
+    {
+        when(speedometer.getSpeed()).thenReturn(10.0);
+        when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
+        engineController.adjustGear();
+        String expectedLogPattern = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} Gear changed to FIRST";
+        verify(logger).log(matches(expectedLogPattern));
+        verifyNoMoreInteractions(logger);
+    }
+
+    //e) The adjustGear method correctly assigns the new gear (setGear() method).
+    @Test
+    public void testAdjustGearSetsNewGear() 
+    {
+        when(speedometer.getSpeed()).thenReturn(10.0);
+        when(time.getCurrentTime()).thenReturn(new Timestamp(System.currentTimeMillis()));
+        engineController.adjustGear();
+        verify(gearbox).setGear(GearValues.FIRST);
+        verifyNoMoreInteractions(gearbox);
     }
 }
