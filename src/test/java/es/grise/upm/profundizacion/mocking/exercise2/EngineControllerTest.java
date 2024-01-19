79eba7406f1ee44bed9140f7f2ba25240606276f
poll2000 <105507228+poll2000@users.noreply.github.com>
2024-01-19 19:57:18 +0100

    Resubida

--- src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
+++ src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
@@ -1,74 +1,90 @@
 package es.grise.upm.profundizacion.mocking.exercise2;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
-
 import java.sql.Timestamp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
 public class EngineControllerTest {
+	
+	
+		EngineController engineController;
+		Logger mockLogger;
+		Speedometer mockSpeedometer;
+		Gearbox mockGearbox;
+		Time mockTime;
 
-    private EngineController engineController;
-    private Logger mockLogger;
-    private Speedometer mockSpeedometer;
-    private Gearbox mockGearbox;
-    private Time mockTime;
-
-    @Before
-    public void init() {
+     	@BeforeEach
+     	public void init() {
     	
-        mockLogger = mock(Logger.class);
-        mockSpeedometer = mock(Speedometer.class);
-        mockGearbox = mock(Gearbox.class);
-        mockTime = mock(Time.class);
+    	 	mockLogger = mock(Logger.class);
+        	mockSpeedometer = mock(Speedometer.class);
+        	mockGearbox = mock(Gearbox.class);
+        	mockTime = mock(Time.class);
 
-        engineController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime);
-    }
+        	engineController = new EngineController(mockLogger, mockSpeedometer, mockGearbox, mockTime);
+     	}
+     	
+     	@Test
+	    void testRecordGearFormat() {
 
-    @Test
-    public void testRecordGearFormat() {
-        
-        when(mockTime.getCurrentTime()).thenReturn(new Timestamp(1234567890L));
-        engineController.adjustGear();
-        ArgumentCaptor<String> logArgumentCaptor = ArgumentCaptor.forClass(String.class);
-        verify(mockLogger).log(logArgumentCaptor.capture());
-        String logMessage = logArgumentCaptor.getValue();
-        String expectedFormat = "2009-02-13 23:31:30 Gear changed to .*";
-        assertTrue(logMessage.matches(expectedFormat));
-    }
+	        when(mockTime.getCurrentTime()).thenReturn(new Timestamp(0));
 
-    @Test
-    public void testGetInstantaneousSpeedCalculation() {
-      
-        when(mockSpeedometer.getSpeed()).thenReturn(10.0, 20.0, 30.0);
-       
-        double result = engineController.getInstantaneousSpeed();
-        assertEquals(20.0, result, 0.01);
-    }
+	        engineController.recordGear(GearValues.FIRST);
+	        String formato = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} Gear changed to FIRST";
+	        verify(mockLogger).log(matches(formato));
+	        verifyNoMoreInteractions(mockLogger);
+	    }
 
-    @Test
-    public void testAdjustGearInvokesGetInstantaneousSpeedThreeTimes() {
-       
-        engineController.adjustGear();
-        verify(mockSpeedometer, times(3)).getSpeed();
-    }
+	    @Test
+	    void testGetInstantaneousSpeedCalculation() {
+	    
+	    	when(mockSpeedometer.getSpeed()).thenReturn(10.0, 20.0, 30.0);
+	        
+	        double result = engineController.getInstantaneousSpeed();
+	        assertEquals(20.0, result, 0.01);
+	    }
 
-    @Test
-    public void testAdjustGearLogsNewGear() {
-        
-        engineController.adjustGear();
-        verify(mockLogger).log(anyString());
-    }
+	    @Test
+	    void testAdjustGearInvokesGetInstantaneousSpeedThreeTimes() {
+	        
+	    	when(mockTime.getCurrentTime()).thenReturn(new Timestamp(0));
+	        when(mockSpeedometer.getSpeed()).thenReturn(10.0, 20.0, 30.0);
+	        engineController.adjustGear();
+	        verify(mockSpeedometer, times(3)).getSpeed();
+	        verifyNoMoreInteractions(mockSpeedometer);
+	        
+	    }
 
-    @Test
-    public void testAdjustGearSetsCorrectNewGear() {
-        
-        when(mockSpeedometer.getSpeed()).thenReturn(15.0);
-        engineController.adjustGear();
-        verify(mockGearbox).setGear(GearValues.FIRST);
-    }
+	    @Test
+	    void testAdjustGearLogsNewGear() {
+
+	    	when(mockTime.getCurrentTime()).thenReturn(new Timestamp(0));
+	        when(mockSpeedometer.getSpeed()).thenReturn(10.0, 20.0, 30.0);
+	        
+	        engineController.adjustGear();
+	        String formato = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} Gear changed to FIRST";
+	        verify(mockLogger).log(matches(formato));
+	        verifyNoMoreInteractions(mockLogger);
+	    }
+
+	    @Test
+	    void testAdjustGearSetsCorrectNewGear() {
+
+	    	when(mockTime.getCurrentTime()).thenReturn(new Timestamp(0));
+	        when(mockSpeedometer.getSpeed()).thenReturn(10.0, 20.0, 30.0);
+	        
+	        engineController.adjustGear();
+	        verify(mockGearbox).setGear(GearValues.FIRST);
+	        verifyNoMoreInteractions(mockGearbox);
+	    }
 }
