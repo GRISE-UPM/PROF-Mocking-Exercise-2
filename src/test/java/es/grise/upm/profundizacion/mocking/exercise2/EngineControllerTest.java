7129a918c504ee5428cbbbd4428700fa9f1c86c0
ThePerkinrex <theperkinrex@gmail.com>
2025-11-15 17:39:02 +0100

    add parametrized test and generalize tests

--- src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
+++ src/test/java/es/grise/upm/profundizacion/mocking/exercise2/EngineControllerTest.java
@@ -1,114 +1,97 @@
 package es.grise.upm.profundizacion.mocking.exercise2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.beans.Transient;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class EngineControllerTest {
 
+    Logger logger;
+    Speedometer speedometer;
+    Gearbox gearbox;
+    Time time;
+    EngineController engineController;
+
+    @BeforeEach
+    void setUp() {
+        logger = mock(Logger.class);
+        speedometer = mock(Speedometer.class);
+        gearbox = mock(Gearbox.class);
+        time = mock(Time.class);
+        engineController = new EngineController(logger, speedometer, gearbox, time);
+        when(time.getCurrentTime()).thenReturn(Timestamp.valueOf("2025-11-15 17:00:00"));
+    }
+
+    static Stream<Arguments> logs() {
+        return Stream.of(
+                arguments("2025-11-15 17:00:00", GearValues.FIRST, "2025-11-15 17:00:00 Gear changed to FIRST"),
+                arguments("2025-11-15 17:00:00", GearValues.SECOND, "2025-11-15 17:00:00 Gear changed to SECOND"),
+                arguments("2023-11-15 17:00:00", GearValues.SECOND, "2023-11-15 17:00:00 Gear changed to SECOND"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("logs")
+    void logFormatTestFirst(String timeString, GearValues gear, String res) {
+        when(time.getCurrentTime()).thenReturn(Timestamp.valueOf(timeString));
+
+        engineController.recordGear(gear);
+        verify(logger).log(res);
+    }
+
     @Test
-	void LogFormatTest() {
-        Logger logger = Mockito.mock(Logger.class);
-        Speedometer speedometer = Mockito.mock(Speedometer.class);
-        Gearbox gearbox = Mockito.mock(Gearbox.class);
-        Time time = new Time();
-
-        ArgumentCaptor<String> valueCapture = ArgumentCaptor.forClass(String.class);
-        doNothing().when(logger).log(valueCapture.capture());
-
-        EngineController engineController =
-                new EngineController(logger, speedometer, gearbox, time);
-
-        engineController.recordGear(GearValues.FIRST);
-
-        //Correct format is "YYYY-MM-DD HH-MM-SS Gear changed to FIRST"
-        assertTrue(valueCapture.getValue().matches(
-                "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} Gear changed to FIRST"));
-	}
-
-    @Test
-    void CorrectInstantaneousSpeedTest(){
-        Logger logger = Mockito.mock(Logger.class);
-        Speedometer speedometer = Mockito.mock(Speedometer.class);
-        Gearbox gearbox = Mockito.mock(Gearbox.class);
-        Time time = new Time();
+    void correctInstantaneousSpeedTest() {
 
         when(speedometer.getSpeed()).thenReturn(30.0);
-
-        EngineController engineController =
-                new EngineController(logger, speedometer, gearbox, time);
 
         assertEquals(30.0, engineController.getInstantaneousSpeed());
     }
 
     @Test
-    void AdjustGear3TimesInvokesInstSpeedTest(){
-        Logger logger = mock(Logger.class);
-        Speedometer speedometer = mock(Speedometer.class);
-        Gearbox gearbox = mock(Gearbox.class);
-        Time time = new Time();
+    void correctMultipleInstantaneousSpeedTest() {
+
+        when(speedometer.getSpeed()).thenReturn(20.0, 30.0, 40.0);
+
+        assertEquals(30.0, engineController.getInstantaneousSpeed());
+    }
+
+    @Test
+    void adjustGear3TimesInvokesInstSpeedTest() {
 
         when(speedometer.getSpeed()).thenReturn(10.0);
 
-        EngineController engineController =
-                new EngineController(logger, speedometer, gearbox, time);
-        EngineController spyController = spy(engineController);
-
-        spyController.adjustGear();
-
-        //TODO: el test pide verificar que getInstantaneousSpeed
-        // se ejecuta 3 veces cuando lo hace 1 vez
-        verify(spyController, times(1)).getInstantaneousSpeed();
-
-        //TODO: lo que si se ejecuta 3 veces es getSpeed dentro de getInstantaneousSpeed
+        engineController.getInstantaneousSpeed();
         verify(speedometer, times(3)).getSpeed();
     }
 
     @Test
-    void AdjustGearInvokesRecordGearTest(){
-        Logger logger = mock(Logger.class);
-        Speedometer speedometer = mock(Speedometer.class);
-        Gearbox gearbox = mock(Gearbox.class);
-        Time time = new Time();
-
+    void adjustGearInvokesRecordGearTest() {
         when(speedometer.getSpeed()).thenReturn(10.0);
 
-        EngineController engineController =
-                new EngineController(logger, speedometer, gearbox, time);
-        EngineController spyController = spy(engineController);
+        engineController = spy(engineController);
 
-        spyController.adjustGear();
+        engineController.adjustGear();
 
-        verify(spyController, times(1)).recordGear(any());
-
+        verify(engineController, times(1)).recordGear(any());
     }
 
     @Test
-    void AdjustGearInvokesSetGearTest(){
-        Logger logger = mock(Logger.class);
-        Speedometer speedometer = mock(Speedometer.class);
-        Gearbox gearbox = mock(Gearbox.class);
-        Time time = new Time();
+    void adjustGearInvokesSetGearTest() {
 
         when(speedometer.getSpeed()).thenReturn(10.0);
 
-        EngineController engineController =
-                new EngineController(logger, speedometer, gearbox, time);
-        EngineController spyController = spy(engineController);
+        engineController = spy(engineController);
 
-        spyController.adjustGear();
+        engineController.adjustGear();
 
-        verify(spyController, times(1)).setGear(any());
+        verify(engineController, times(1)).setGear(any());
     }
 }
